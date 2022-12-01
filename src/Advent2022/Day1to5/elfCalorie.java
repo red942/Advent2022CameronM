2526e6059715eb9abb417dfd630f885202c1a9a3
red942 <cameron.miller014@topper.wku.edu>
2022-12-01 15:18:24 -0600

    first commit

---
+++ src/Advent2022/Day1to5/elfCalorie.java
@@ -0,0 +1,33 @@
+package Advent2022.Day1to5;
+import java.util.Scanner;
+
+/*code finished by Cameron Miller 10:39AM 12/1/2022 (CST), this is day 1, challenge 1*/
+/*for the input, paste in the values to the terminal and then type a new line saying "done" after a blank line*/
+
+public class elfCalorie {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int elfHigh = 0;
+        int elfNum = 0;
+        int elfNumTemp = 0;
+        int elfTemp = 0;
+        while (true){
+            String num = stdin.nextLine();
+            if (num.equals("done"))
+                break;
+            if (num.equals("")){
+                elfNumTemp++;
+                if (elfTemp > elfHigh){
+                    elfHigh = elfTemp;
+                    elfNum = elfNumTemp;
+                }
+                elfTemp = 0;
+                continue;
+            }
+            elfTemp += Integer.valueOf(num);
+        }
+        System.out.println(elfNum);
+        System.out.println(elfHigh);
+        stdin.close();
+    }
+}
---
+++ src/Advent2022/Day1to5/elfCalorie2.java
@@ -0,0 +1,35 @@
+package Advent2022.Day1to5;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/*code finished by Cameron Miller 10:54AM 12/1/2022 (CST), this is day 1, challenge 2*/
+/*for the input, paste in the values to the terminal and then type a new line saying "done" after a blank line */
+
+public class elfCalorie2 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int[] elfHighs = new int[3];
+        int elfTemp = 0;
+        while (true){
+            Arrays.sort(elfHighs);
+            String num = stdin.nextLine();
+            if (num.equals("done"))
+                break;
+            if (num.equals("")){
+
+                if (elfTemp > elfHighs[0]){
+                    elfHighs[0] = elfTemp;
+                }
+                elfTemp = 0;
+                continue;
+            }
+            elfTemp += Integer.valueOf(num);
+        }
+        System.out.println(Arrays.toString(elfHighs));
+        int sum = 0;
+        for (int n : elfHighs)
+            sum += n;
+        System.out.println(sum);
+        stdin.close();
+    }
+}
